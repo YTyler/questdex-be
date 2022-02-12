72ffe23feb08741a34777e98b7e116974c6bd446
YTyler <tyates907@gmail.com>
2022-02-11 19:15:09 -0800

    first commit

---
+++ src/main/java/com/ytyler/questdexbe/QuestdexBeApplication.java
@@ -0,0 +1,13 @@
+package com.ytyler.questdexbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestdexBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestdexBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ytyler/questdexbe/QuestdexBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytyler.questdexbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestdexBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
